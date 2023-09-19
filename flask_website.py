46c3fa550b1df9d98094afe35c771f8a02be18ab
caixukun <datehoer@gmail.com>
2023-09-19 15:12:58 +0800

    增加部分功能，修改前端页面bug

--- flask_website.py
+++ flask_website.py
@@ -1,16 +1,18 @@
 import uvicorn
 import subprocess
+import threading
+import psutil
+from typing import List
 from fastapi import FastAPI, BackgroundTasks, Request
 from pydantic import BaseModel
 from sqlalchemy import create_engine, text
 from ast import literal_eval
-import threading
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-engine = create_engine('sqlite:///tasks.db', connect_args={"check_same_thread": False}, echo=True)
+engine = create_engine('sqlite:///tasks.db', connect_args={"check_same_thread": False}, echo=True, pool_size=10, max_overflow=20)
 task_lock = threading.Lock()
 app.add_middleware(
     CORSMiddleware,
@@ -64,13 +66,14 @@ def execute_task_in_subprocess(task_id, script_path, script_args):
             tran.commit()
             tran.close()
         process.wait()
+        stdout_data, stderr_data = process.communicate()
         result = {
-            "stdout": process.stdout,
-            "stderr": process.stderr
+            "stdout": str(stdout_data),
+            "stderr": str(stderr_data)
         }
         with engine.connect() as conn:
             tran = conn.begin()
-            conn.execute(text("UPDATE tasks SET status=:status, result=:result WHERE id=:id"),
+            conn.execute(text("UPDATE tasks SET status=:status, result=:result,subprocess_pid = NULL  WHERE id=:id"),
                          {"status": "结束", "result": str(result), "id": task_id})
             tran.commit()
             tran.close()
@@ -78,10 +81,50 @@ def execute_task_in_subprocess(task_id, script_path, script_args):
         error_message = str(e)
         with engine.connect() as conn:
             tran = conn.begin()
-            conn.execute(text("UPDATE tasks SET status=:status, result=:error_message WHERE id=:id"),
+            conn.execute(text("UPDATE tasks SET status=:status, result=:error_message,subprocess_pid = NULL WHERE id=:id"),
                          {"status": "失败", "error_message": str({"stdout": "", "stderr": error_message}), "id": task_id})
             tran.commit()
             tran.close()
+
+
+def get_process_status(pid):
+    try:
+        process = psutil.Process(pid)
+        return {"code": 0, "message": process.status(), "status": "进行中"}
+    except psutil.NoSuchProcess:
+        return {"code": 1, "message": "进程不存在", "status": "结束"}
+    except Exception as e:
+        return {"code": 2, "message": f"未知错误: {str(e)}", "status": "错误"}
+
+
+@app.post("/execute_selected_tasks")
+async def execute_selected_tasks(task_ids: dict, background_tasks: BackgroundTasks):
+    global task_lock
+    for task_id in task_ids['task_ids']:
+        with task_lock:
+            with engine.connect() as conn:
+                tran = conn.begin()
+                task_info = conn.execute(
+                    text("SELECT script_path, script_args, status FROM tasks WHERE id=:id"),
+                    {"id": task_id}
+                ).fetchone()
+                tran.commit()
+                tran.close()
+            if task_info and (task_info[2] == "未开始" or task_info[2] == "结束"):
+                background_tasks.add_task(execute_task_in_subprocess, task_id, task_info[0], task_info[1])
+
+    return {"message": "选中的任务已放入后台执行"}
+
+
+@app.post("/delete_selected_tasks")
+async def delete_selected_tasks(task_ids: dict):
+    with engine.connect() as conn:
+        for task_id in task_ids['task_ids']:
+            tran = conn.begin()
+            conn.execute(text("DELETE FROM tasks WHERE id=:id"), {"id": task_id})
+            tran.commit()
+
+    return {"message": "选中的任务已删除"}
 
 
 @app.post("/create_task")
@@ -124,9 +167,24 @@ async def execute_task(task_id: int, background_tasks: BackgroundTasks):
 async def check_status(task_id: int):
     with engine.connect() as conn:
         tran = conn.begin()
-        status = conn.execute(text("SELECT status FROM tasks WHERE id=:id"), {"id": task_id}).fetchone()
+        status = conn.execute(text("SELECT status, subprocess_pid FROM tasks WHERE id=:id"), {"id": task_id}).fetchone()
         tran.commit()
         tran.close()
+        task_status = status[0]
+        subprocess_pid = status[1]
+    if task_status == "进行中":
+        if subprocess_pid is not None:
+            process_status = get_process_status(subprocess_pid)
+            with engine.connect() as conn:
+                tran = conn.begin()
+                conn.execute(text("UPDATE tasks SET status=:status, result=:result WHERE id=:id"),
+                             {"status": process_status["status"], "result": {'stdout': process_status["message"], "stderr": ""}, "id": task_id})
+                tran.commit()
+                tran.close()
+            if process_status["code"] == 0:
+                return {"status": process_status["status"]}
+            else:
+                return {"status": process_status["status"]}
     if status:
         return {"status": status[0]}
     else:
@@ -137,30 +195,60 @@ async def check_status(task_id: int):
 async def get_result(task_id: int):
     with engine.connect() as conn:
         tran = conn.begin()
-        result = conn.execute(text("SELECT result FROM tasks WHERE id=:id"), {"id": task_id}).fetchone()
+        result = conn.execute(text("SELECT result, subprocess_pid FROM tasks WHERE id=:id"), {"id": task_id}).fetchone()
         tran.commit()
         tran.close()
-    if result:
-        parsed_result = literal_eval(result[0])
+        task_status = result[0]
+        subprocess_pid = result[1]
+    if task_status is not None:
+        parsed_result = literal_eval(task_status)
+        if task_status == "进行中" and subprocess_pid is not None:
+            process_status = get_process_status(subprocess_pid)
+            with engine.connect() as conn:
+                tran = conn.begin()
+                conn.execute(text("UPDATE tasks SET status=:status, result=:result WHERE id=:id"),
+                             {"status": process_status["status"],
+                              "result": {'stdout': process_status["message"], "stderr": ""}, "id": task_id})
+                tran.commit()
+                tran.close()
+            if process_status["code"] == 0:
+                return {"status": process_status["status"], "result": {'stdout': process_status["message"], "stderr": ""}}
+            else:
+                return {"status": process_status["status"], "result": {"stderr": "任务未开始/不存在"}}
         return {"result": parsed_result}
     else:
-        return {"error": "任务不存在"}
+        return {"result": {"stderr": "任务未开始/不存在"}}
 
 
 @app.get("/get_all_tasks")
 async def get_all_tasks():
     with engine.connect() as conn:
         tran = conn.begin()
-        task_result = conn.execute(text("SELECT id, script_path, script_args, status FROM tasks")).fetchall()
+        task_result = conn.execute(text("SELECT id, script_path, script_args, status, subprocess_pid FROM tasks")).fetchall()
         tran.commit()
         tran.close()
     all_tasks = []
     for task in task_result:
+        task_status = task[3]
+        subprocess_pid = task[4]
+        if subprocess_pid is not None:
+            process_status = get_process_status(subprocess_pid)
+            if process_status["code"] == 0:
+                task_status = process_status["status"]
+                with engine.connect() as conn:
+                    tran = conn.begin()
+                    conn.execute(text("UPDATE tasks SET status=:status, result=:result WHERE id=:id"),
+                                 {"status": process_status["status"],
+                                  "result": {'stdout': process_status["message"], "stderr": ""}, "id": task[0]})
+                    tran.commit()
+                    tran.close()
+            else:
+                task_status = process_status["status"]
         all_tasks.append({
             "id": task[0],
             "script_path": task[1],
             "script_args": task[2],
-            "status": task[3]
+            "status": task_status
         })
     return all_tasks
 
--- get_baidu.py
+++ get_baidu.py
@@ -1,4 +1,14 @@
-import requests
+import psutil
 
-res =requests.get('http://www.baidu.com')
-print(res.text)
+def get_process_status(pid):
+    try:
+        process = psutil.Process(pid)
+        return process.status()
+    except psutil.NoSuchProcess:
+        return "进程不存在"
+    except Exception as e:
+        return f"未知错误: {str(e)}"
+
+# 使用示例
+status = get_process_status(84208)  # 假设1234是你想查询的PID
+print(f"进程状态: {status}")
